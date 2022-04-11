14cbb938d40f7a09e578fa47506a793bea197036
Lucas Alegre <lucasnale@gmail.com>
2022-04-11 17:38:11 -0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+# Environment-specific dependencies.
+extras = {}
+
+setup(
+    name="mo-gym",
+    version="0.1",
+    description="MO-Gym: Environments for Multi-Objective RL.",
+    url="https://www.github.com/LucasAlegre/mo-gym",
+    author="LucasAlegre",
+    author_email="lnalegre@inf.ufrgs.br",
+    license="MIT",
+    packages=["mo_gym"],
+    install_requires=[
+        "gym",
+        "numpy",
+    ],
+    extras_require=extras,
+    tests_require=["pytest", "mock"],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
