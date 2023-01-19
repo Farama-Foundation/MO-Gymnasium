eaef625d1ac278cbbb2175a036486dbf69b6e6b5
Lucas Alegre <lucasnale@gmail.com>
2023-01-19 16:57:07 +0100

    Convert highway env observation from gym to gymnasium

--- mo_gymnasium/envs/highway/highway.py
+++ mo_gymnasium/envs/highway/highway.py
@@ -1,5 +1,17 @@
 import numpy as np
-from gymnasium.spaces import Box
+import gym
+import gymnasium
+from gymnasium.spaces import (
+    Box,
+    Dict,
+    Discrete,
+    Graph,
+    MultiBinary,
+    MultiDiscrete,
+    Sequence,
+    Text,
+    Tuple,
+)
 from highway_env.envs import HighwayEnv, HighwayEnvFast
 
 
@@ -9,6 +21,8 @@ class MOHighwayEnv(HighwayEnv):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.reward_space = Box(low=-1.0, high=1.0, shape=(3,), dtype=np.float32)
+        self.observation_space = _convert_space(self.observation_space)
+        self.action_space = _convert_space(self.action_space)
 
     def step(self, action):
         obs, reward, terminated, truncated, info = super().step(action)
@@ -32,6 +46,8 @@ class MOHighwayEnvFast(HighwayEnvFast):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.reward_space = Box(low=-1.0, high=1.0, shape=(3,), dtype=np.float32)
+        self.observation_space = _convert_space(self.observation_space)
+        self.action_space = _convert_space(self.action_space)
 
     def step(self, action):
         obs, reward, terminated, truncated, info = super().step(action)
@@ -47,3 +63,41 @@ class MOHighwayEnvFast(HighwayEnvFast):
         vec_reward *= rewards["on_road_reward"]
         info["original_reward"] = reward
         return obs, vec_reward, terminated, truncated, info
+
+
+def _convert_space(space: gym.Space) -> gymnasium.Space:
+    """Converts a gym space to a gymnasium space.
+    Args:
+        space: the space to convert
+    Returns:
+        The converted space
+    """
+    if isinstance(space, gym.spaces.Discrete):
+        return Discrete(n=space.n)
+    elif isinstance(space, gym.spaces.Box):
+        return Box(low=space.low, high=space.high, shape=space.shape, dtype=space.dtype)
+    elif isinstance(space, gym.spaces.MultiDiscrete):
+        return MultiDiscrete(nvec=space.nvec)
+    elif isinstance(space, gym.spaces.MultiBinary):
+        return MultiBinary(n=space.n)
+    elif isinstance(space, gym.spaces.Tuple):
+        return Tuple(spaces=tuple(map(_convert_space, space.spaces)))
+    elif isinstance(space, gym.spaces.Dict):
+        return Dict(spaces={k: _convert_space(v) for k, v in space.spaces.items()})
+    elif isinstance(space, gym.spaces.Sequence):
+        return Sequence(space=_convert_space(space.feature_space))
+    elif isinstance(space, gym.spaces.Graph):
+        return Graph(
+            node_space=_convert_space(space.node_space),  # type: ignore
+            edge_space=_convert_space(space.edge_space),  # type: ignore
+        )
+    elif isinstance(space, gym.spaces.Text):
+        return Text(
+            max_length=space.max_length,
+            min_length=space.min_length,
+            charset=space._char_str,
+        )
+    else:
+        raise NotImplementedError(
+            f"Cannot convert space of type {space}. Please upgrade your code to gymnasium."
+        )
