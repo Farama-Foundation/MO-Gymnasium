3bb3ce971e5dfe65bbebaaacb3a3a5e9770b10c0
Lucas Alegre <lucasnale@gmail.com>
2023-02-13 11:27:28 +0100

    Automatic generation of tests (#37)
    * Make tests parameterizable
    
    * Add nondeterministic flag to mo-highway
    
    * Fix minecart observation bounds
    
    * Fix water-reservoir observation dtype

--- mo_gymnasium/envs/highway/__init__.py
+++ mo_gymnasium/envs/highway/__init__.py
@@ -1,6 +1,6 @@
 from gymnasium.envs.registration import register
 
 
-register(id="mo-highway-v0", entry_point="mo_gymnasium.envs.highway.highway:MOHighwayEnv")
+register(id="mo-highway-v0", entry_point="mo_gymnasium.envs.highway.highway:MOHighwayEnv", nondeterministic=True)
 
-register(id="mo-highway-fast-v0", entry_point="mo_gymnasium.envs.highway.highway:MOHighwayEnvFast")
+register(id="mo-highway-fast-v0", entry_point="mo_gymnasium.envs.highway.highway:MOHighwayEnvFast", nondeterministic=True)
