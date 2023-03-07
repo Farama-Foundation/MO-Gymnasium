3876d9e797093680d59e0735c5e8e9d05ff30c80
Florian Felten <florian.felten@uni.lu>
2023-03-07 09:20:52 +0100

    Move evaluation utils to baselines

--- mo_gymnasium/__init__.py
+++ mo_gymnasium/__init__.py
@@ -4,15 +4,6 @@
 import mo_gymnasium.envs
 
 # Utils
-# TODO this might be disgusting in the long run
-from mo_gymnasium.evaluation import (
-    hypervolume,  # TODO we might consider removing HV from the repo, depends more on the algos than the environments
-)
-from mo_gymnasium.evaluation import (
-    eval_mo,
-    eval_mo_reward_conditioned,
-    policy_evaluation_mo,
-)
 from mo_gymnasium.utils import (
     LinearReward,
     MOClipReward,
