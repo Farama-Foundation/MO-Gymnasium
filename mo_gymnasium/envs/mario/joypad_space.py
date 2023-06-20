d8557b46c6efe012a78e1e48995e2aafb0ade4c3
Lucas Alegre <lucasnale@gmail.com>
2023-06-20 15:00:30 +0200

    Remove reset of JoyPad space

--- mo_gymnasium/envs/mario/joypad_space.py
+++ mo_gymnasium/envs/mario/joypad_space.py
@@ -72,10 +72,6 @@ class JoypadSpace(Wrapper):
         # take the step and record the output
         return self.env.step(self._action_map[action])
 
-    def reset(self):
-        """Reset the environment and return the initial observation."""
-        return self.env.reset()
-
     def get_keys_to_action(self):
         """Return the dictionary of keyboard keys to actions."""
         # get the old mapping of keys to actions
