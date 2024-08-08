0ec9b86c789afd1b393d67d16aa52d10870f0e90
Pranav Gupta <praanavgupta@gmail.com>
2024-08-08 08:22:49 +0100

    Adding three different mo-mountain-car environments (#97)
    Co-authored-by: Lucas Alegre <lucas.nunesalegre@disneyresearch.com>

--- mo_gymnasium/envs/mountain_car/__init__.py
+++ mo_gymnasium/envs/mountain_car/__init__.py
@@ -6,3 +6,24 @@ register(
     entry_point="mo_gymnasium.envs.mountain_car.mountain_car:MOMountainCar",
     max_episode_steps=200,
 )
+
+register(
+    id="mo-mountaincar-3d-v0",
+    entry_point="mo_gymnasium.envs.mountain_car.mountain_car:MOMountainCar",
+    max_episode_steps=200,
+    kwargs={"add_speed_objective": True, "merge_move_penalty": True},
+)
+
+register(
+    id="mo-mountaincar-timemove-v0",
+    entry_point="mo_gymnasium.envs.mountain_car.mountain_car:MOMountainCar",
+    max_episode_steps=200,
+    kwargs={"merge_move_penalty": True},
+)
+
+register(
+    id="mo-mountaincar-timespeed-v0",
+    entry_point="mo_gymnasium.envs.mountain_car.mountain_car:MOMountainCar",
+    max_episode_steps=200,
+    kwargs={"remove_move_penalty": True, "add_speed_objective": True},
+)
