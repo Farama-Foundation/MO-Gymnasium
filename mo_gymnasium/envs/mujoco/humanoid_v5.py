5b248edf2149ab07953fe057dc9dfa5d3ecfdf23
Lucas Alegre <lucasnale@gmail.com>
2024-10-28 12:21:22 -0300

    Add Mujoco v5 environments (#85)
    Co-authored-by: Lucas Alegre <lucasnale@gmail.com>
    Co-authored-by: Mark Towers <mark.m.towers@gmail.com>
    Co-authored-by: Florian Felten <felten.florian@hotmail.fr>

---
+++ mo_gymnasium/envs/mujoco/humanoid_v5.py
@@ -0,0 +1,44 @@
+import numpy as np
+from gymnasium.envs.mujoco.humanoid_v5 import HumanoidEnv
+from gymnasium.spaces import Box
+from gymnasium.utils import EzPickle
+
+
+class MOHumanoidEnv(HumanoidEnv, EzPickle):
+    """
+    ## Description
+    Multi-objective version of the HumanoidEnv environment.
+
+    See [Gymnasium's env](https://gymnasium.farama.org/environments/mujoco/humanoid/) for more information.
+
+    The original Gymnasium's 'Humanoid-v5' is recovered by the following linear scalarization:
+
+    env = mo_gym.make('mo-humanoid-v5')
+    LinearReward(env, weight=np.array([1.25, 0.1]))
+
+    ## Reward Space
+    The reward is 2-dimensional:
+    - 0: Reward for running forward (x-velocity)
+    - 1: Control cost of the action
+
+    ## Version History:
+    - v5: Now includes contact forces. See: https://gymnasium.farama.org/environments/mujoco/humanoid/#version-history
+          The scales of the control cost has changed from v4.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        EzPickle.__init__(self, **kwargs)
+        self.reward_space = Box(low=-np.inf, high=np.inf, shape=(2,))
+        self.reward_dim = 2
+
+    def step(self, action):
+        observation, reward, terminated, truncated, info = super().step(action)
+        velocity = info["x_velocity"]
+        neg_energy_cost = info["reward_ctrl"] / self._ctrl_cost_weight  # Revert the scale applied in the original environment
+        vec_reward = np.array([velocity, neg_energy_cost], dtype=np.float32)
+
+        vec_reward += self.healthy_reward  # All objectives are penalyzed when the agent falls
+        vec_reward += info["reward_contact"]  # Do not treat contact forces as a separate objective
+
+        return observation, vec_reward, terminated, truncated, info
--- mo_gymnasium/envs/mujoco/walker2d_v5.py
+++ mo_gymnasium/envs/mujoco/walker2d_v5.py
@@ -1,5 +1,5 @@
 import numpy as np
-from gymnasium.envs.mujoco.walker2d_v4 import Walker2dEnv
+from gymnasium.envs.mujoco.walker2d_v5 import Walker2dEnv
 from gymnasium.spaces import Box
 from gymnasium.utils import EzPickle
 
@@ -11,10 +11,18 @@ class MOWalker2dEnv(Walker2dEnv, EzPickle):
 
     See [Gymnasium's env](https://gymnasium.farama.org/environments/mujoco/walker2d/) for more information.
 
+    The original Gymnasium's 'Walker2d-v5' is recovered by the following linear scalarization:
+
+    env = mo_gym.make('mo-walker2d-v5')
+    LinearReward(env, weight=np.array([1.0, 1e-3]))
+
     ## Reward Space
     The reward is 2-dimensional:
     - 0: Reward for running forward (x-velocity)
     - 1: Control cost of the action
+
+    # Version History
+    - See https://gymnasium.farama.org/main/environments/mujoco/walker2d/#version-history
     """
 
     def __init__(self, **kwargs):
@@ -26,9 +34,9 @@ class MOWalker2dEnv(Walker2dEnv, EzPickle):
     def step(self, action):
         observation, reward, terminated, truncated, info = super().step(action)
         velocity = info["x_velocity"]
-        energy = -np.sum(np.square(action))
+        neg_energy_cost = info["reward_ctrl"] / self._ctrl_cost_weight
 
-        vec_reward = np.array([velocity, energy], dtype=np.float32)
+        vec_reward = np.array([velocity, neg_energy_cost], dtype=np.float32)
 
         vec_reward += self.healthy_reward  # All objectives are penalyzed when the agent falls
 
