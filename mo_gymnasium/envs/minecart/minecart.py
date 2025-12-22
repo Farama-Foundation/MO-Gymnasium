3ee13320e0902d16798d30db93ebb6644c13ac9e
LucasAlegre <lucasnale@gmail.com>
2025-12-22 17:26:40 -0300

    Fix numpy.math deprecated in minecart

--- mo_gymnasium/envs/minecart/minecart.py
+++ mo_gymnasium/envs/minecart/minecart.py
@@ -786,7 +786,7 @@ def compute_angle(p0, p1, p2):
     v0 = np.array(p0) - np.array(p1)
     v1 = np.array(p2) - np.array(p1)
 
-    angle = np.math.atan2(np.linalg.det([v0, v1]), np.dot(v0, v1))
+    angle = np.arctan2(np.linalg.det([v0, v1]), np.dot(v0, v1))
     return np.degrees(angle)
 
 
